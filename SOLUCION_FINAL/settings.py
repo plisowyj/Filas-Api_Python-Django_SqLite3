cfef4311c804c669bbd1d350620bdb4fe1b9ea72
Pablo Lisowyj <plisowyj@gmail.com>
2022-08-09 21:48:38 -0300

    pl

--- SOLUCION_FINAL/settings.py
+++ SOLUCION_FINAL/settings.py
@@ -137,8 +137,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
-os.makedirs(STATIC_ROOT,exists_ok=True)
-os.makedirs(STATIC_TMP,exists_ok=True)
+os.makedirs(STATIC_ROOT,exist_ok=True)
+os.makedirs(STATIC_TMP,exist_ok=True)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static')

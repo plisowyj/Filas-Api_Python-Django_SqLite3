8395324cff21ed3b3e524c1f967b23532b1feeae
Pablo Lisowyj <plisowyj@gmail.com>
2022-08-12 20:30:03 -0300

    hola

--- anuncio_turnos/urls.py
+++ anuncio_turnos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('caller/', views.caller, name='caller'),
-    
+    path('anunws/', views.callerws.as_view()),
 ]
--- anuncio_turnos/views.py
+++ anuncio_turnos/views.py
@@ -1,13 +1,33 @@
 from django.http import HttpResponse
 from django.template import loader
 from admin_turnos.models import Anuncios
+from django.views import View
+from django.http import JsonResponse
 
 def caller(request):
-  form = Anuncios.objects.all()
-  first = form.first()
   template = loader.get_template('anuncio_turnos/caller.html')
-  context = {"form":form,"first":first}
-  return HttpResponse(template.render(context, request))
+  
+  return HttpResponse(template.render({}, request))
 
 
+class callerws(View):
+    def get(self, request):
+
+        items = Anuncios.objects.all()[:15]
+
+        tabla = '<table class = "table table-striped text-uppercase" style = "width: 85%;">'
+        i = 0
+        for item in items:
+            tabla = tabla + '<tr>'
+            if i==0:
+              tabla = tabla + '<td class = "text-center" style = "width: 30%; background-color: aquamarine;font-size: 90px;font-weight: bold;">'+ item.decorador.upper() + str(item.contador) +'</td><td style="width: 30%; background-color: aquamarine;font-size: 90px;font-weight: bold;">'+item.lugar.upper()+'</td>'
+            else:
+              tabla = tabla + '<td class = "text-center" style = "width: 30%; background-color: beige;font-size: 50px;font-weight: bold;">'+ item.decorador.upper() + str(item.contador) +' </td><td style = "width: 30%; background-color: beige;font-size: 50px;font-weight: bold;" >'+item.lugar.upper()+'</td>'
+
+            tabla = tabla + '</tr>'
+            i += 1
+        
+        tabla = tabla+'</table>'
+        data = {'table': tabla}
+        return JsonResponse(data, status=201)
 

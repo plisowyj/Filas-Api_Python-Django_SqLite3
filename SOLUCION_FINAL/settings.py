cd9049852491634b3da03ca905c15e6ac83c489b
Pablo Lisowyj <plisowyj@gmail.com>
2022-08-12 14:45:10 -0300

    pl

--- SOLUCION_FINAL/settings.py
+++ SOLUCION_FINAL/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 #ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['<your app URL without the https:// prefix>.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://gestion-filas.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
